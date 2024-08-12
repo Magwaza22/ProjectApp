8938bd43416e7a656b46aa64230e43a1f3b967fa
Magwaza22 <127666759+Magwaza22@users.noreply.github.com>
2024-08-12 14:01:28 +0200

    first page commit

---
+++ src/main/java/firstPageGui.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class firstPageGui extends JFrame implements ActionListener {
+    private JPanel panelCenter, panelSouth, panelNorth;
+    private JLabel lblHeading, lblAbout, lblCputApp, lblServices, lblShortText;
+    private JButton BtnApply;
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
